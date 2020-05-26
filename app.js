d545e133d417e524e700ba668689a7c1c689ad9f
Joe Webster <joseph.scott.webster@gmail.com>
2020-05-26 13:58:02 -0400

    html timer seconds loop at 00 and minutes decrease

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
-function countdownFrom(seconds) {
-  console.log(seconds);
+const displayMinutes = document.querySelector('.min');
+const displaySeconds = document.querySelector('.sec');
+const startStopBtn = document.querySelector('.start-stop');
 
-  setInterval(() => {    
-    seconds--;
-    console.log(seconds);
-  }, 1000)
+function startStopTimer() {
+  let sec = displaySeconds.textContent;
+  let min = displayMinutes.textContent;
+  
+  setInterval(() => {
+    sec--;
+
+    if (sec < 0) {
+      sec = 59;
+      min--;
+    }
+
+    if (sec >= 0 && sec < 10) {
+      sec = "0" + sec;
+    }
+    
+    displaySeconds.textContent = sec;
+    displayMinutes.textContent = min;
+  }, 1000);
+
 }
 
-countdownFrom(60);
+startStopBtn.addEventListener('click', startStopTimer);
