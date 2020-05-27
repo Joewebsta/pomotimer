27783f83282fe07542eeded4710ea09c9aabcc0f
Joe Webster <joseph.scott.webster@gmail.com>
2020-05-27 16:40:23 -0400

    Refactors js and adds reset button and functionality

--- app.js
+++ app.js
@@ -1,47 +1,77 @@
 const displayMinutes = document.querySelector('.min');
 const displaySeconds = document.querySelector('.sec');
 const startPauseBtn = document.querySelector('.start-pause');
-var intervalId;
+const resetBtn = document.querySelector('.reset');
+let sec = 05;
+let min = 1;
+let timerIntervalId;
 
-function pauseTimer() {
-  startPauseBtn.setAttribute('data-mode', 'inactive');
-  startPauseBtn.textContent = 'Start';
+function updateStartPauseBtn() {
+  const timerMode = startPauseBtn.getAttribute(['data-mode']);
+  
+  if( timerMode === 'active') {
+    startPauseBtn.dataset.mode = 'inactive';
+    startPauseBtn.textContent = 'Start';
+  } else {
+    startPauseBtn.dataset.mode = 'active';
+    startPauseBtn.textContent = 'Pause';
+  }
 }
 
 function startTimer() {
-  startPauseBtn.setAttribute('data-mode', 'active');
-  startPauseBtn.textContent = 'Pause';
+  timerIntervalId = setInterval(updateTimerText, 1000);
+  updateStartPauseBtn(); 
 }
 
-function toggleTimer() {
-  let sec = displaySeconds.textContent;
-  let min = displayMinutes.textContent;
-  const timerMode = startPauseBtn.getAttribute(['data-mode']);
+function pauseTimer() {
+  clearInterval(timerIntervalId);
+  updateStartPauseBtn();
+}
 
+function resetTimer() {
+  clearInterval(timerIntervalId);
+  sec = 0;
+  min = 25;
+  formatSingleDigits();
+  displayTime();
+  updateStartPauseBtn();
+}
+
+function displayTime() {
+  displaySeconds.textContent = sec;
+  displayMinutes.textContent = min;
+} 
+
+function formatSingleDigits() {
+  if (sec < 0) {
+    sec = 59;
+    min--;
+  }
+
+  if (sec < 10) {
+    sec = "0" + sec;
+  }
+
+  if (min < 1 && sec === 59) {
+    min = "0" + min;
+  }
+}
+
+function updateTimerText() {
+  sec--;
+  formatSingleDigits();
+  displayTime();
+}
+
+function timerHandler() {
+  const timerMode = startPauseBtn.dataset.mode;
+  
   if (timerMode === 'inactive') {
-    
-    startTimer(); 
-
-    intervalId = setInterval(() => {
-      sec--;
-
-      if (sec < 0) {
-        sec = 59;
-        min--;
-      }
-
-      if (sec >= 0 && sec < 10) {
-        sec = "0" + sec;
-      }
-      
-      displaySeconds.textContent = sec;
-      displayMinutes.textContent = min;
-    }, 1000);
-    
+    startTimer();
   } else {
-    clearInterval(intervalId);
     pauseTimer();
   }
 }
 
-startPauseBtn.addEventListener('click', toggleTimer);
+startPauseBtn.addEventListener('click', timerHandler);
+resetBtn.addEventListener('click', resetTimer);
